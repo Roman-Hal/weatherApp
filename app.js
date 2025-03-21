6c547d23563140de839fa4d44519dd7e0610e64e
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2025-03-21 17:25:35 +0000

    Changes to the app
    major changes to the app

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+//import{ apiKey } from "dotenv";
+/*import { config } from "dotenv";
+import './env';*/
+//import dotenv from "weatherApp/dotenv";
+/*const dotenv = require('dotenv')
+require('dotenv').config();
+const apiKey = process.env.API_KEY;*/
 window.addEventListener("load", ()=> {
     
+
     let lon;
     let lat;
     let appDescription = document.querySelector('.app-name');
+    let feelsDesc = document.querySelector('.feels-description');
+    let feelsTemperature = document.querySelector('.feels-temp');
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
     let iconPlace = document.querySelector('.icon');
     let degreeSection = document.querySelector('.degree-section');
-    const degreeSpan = document.querySelector('.degree-section span');
+    //const degreeSpan = document.querySelectorAll(".degree-section span, .feels-like-temperature span");
+    const degreeSpan = document.querySelector('.temperature span');
+    const feelsSpan = document.querySelector('.feels-like-temperature span');
     let wrong = document.querySelector('.wrong');
     let timeActual = document.querySelector('.time');
+    let dateActual = document.querySelector('.actualDate');
+    /*dotenv.config();*/
+    /*const apiKey = process.env.REACT_APP_API_KEY;
+    console.log(apiKey);*/
+    /*let dateActual = document.querySelector('.date');*/
 
     
 
@@ -29,14 +46,25 @@ window.addEventListener("load", ()=> {
 
                 function myTimer() {
                 var date = new Date();
+                const options = {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  };
                 var time = date.toLocaleTimeString();
+                var actualDate = date.toLocaleDateString("default", options);
                 timeActual.innerHTML = time;
+                dateActual.innerHTML = actualDate;
+                /*dateActual.innerHTML = date.toDateString("cs-CZ", options);*/
                 }
 
             //const proxy = "https://cors-anywhere.herokuapp.com/";
+            
 
             //const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=hourly,daily,minutely&units=imperial&appid=b6cfa4030fe8998f2e47685f8d3e4cf4`
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=b6cfa4030fe8998f2e47685f8d3e4cf4`
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=b6cfa4030fe8998f2e47685f8d3e4cf4`;
+            //const api = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=%REACT_APP_API_KEY%';
             //const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily,minutely&units=imperial&appid=b6cfa4030fe8998f2e47685f8d3e4cf4`
             //http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
 
@@ -47,15 +75,20 @@ window.addEventListener("load", ()=> {
             .then(data => {
                 const {temp} = data.main;
                 //const {temp} = data.current;
+                const {feels_like} = data.main;
                 const {description, icon} = data.weather[0];
                 //const {description, icon} = data.current.weather[0];
                 const {name} = data;
                 //const {timezone} = data;
                 //const tZone = data.timezone;
                 
-                let celsius = Math.round(temp * 10) /10
+                let celsius = Math.round(temp * 10) /10;
+                let feelsCelsius = Math.round(feels_like * 10) /10;
                 //set DOM Elements from the API
                 appDescription.textContent = 'Weather App';
+                //feels like temperature values
+                feelsDesc.textContent = 'feels:';
+                feelsTemperature.textContent = feelsCelsius;
                 //console.log(data.current.weather[0].description);
                 temperatureDegree.textContent = celsius;
                 //temperatureDegree.textContent = temp;
@@ -63,6 +96,7 @@ window.addEventListener("load", ()=> {
                 locationTimezone.textContent = name;
                 //locationTimezone.textContent = timezone;
                 degreeSpan.textContent = '°C';
+                feelsSpan.textContent = degreeSpan.textContent;
                 //degreeSpan.textContent = '°F';
 
                 
@@ -71,11 +105,20 @@ window.addEventListener("load", ()=> {
                 
                 //let celsius = (temp - 32) * (5 / 9);
                 let fahrenheit = (temp * (9/5)) + 32;
+                let feelsFahrenheit = (feels_like * (9/5)) + 32;
 
                 //set icon
                 //iconPlace.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
                 iconPlace.src = `openweathermap/${icon}.svg`;
-
+                //Change pressed temperature button
+                degreeSection.addEventListener('mousedown', () => {
+                    degreeSection.style.backgroundColor = "lightgrey";
+                    degreeSection.style.boxShadow = "1px 1px 4px 4px";
+                });
+                degreeSection.addEventListener('mouseup', () => {
+                    degreeSection.style.backgroundColor = "transparent";
+                    degreeSection.style.boxShadow = "0px 0px 2px 2px grey";
+                });
                 //Change temperature to Celsius/Farenheit
                 degreeSection.addEventListener('click', () => {
                    /* if(degreeSpan.textContent === "°F") {
@@ -87,10 +130,14 @@ window.addEventListener("load", ()=> {
                     }*/
                     if(degreeSpan.textContent === "°C") {
                         degreeSpan.textContent = "°F";
+                        feelsSpan.textContent = degreeSpan.textContent;
                         temperatureDegree.textContent = Math.round(fahrenheit * 100) /100;
+                        feelsTemperature.textContent = Math.round(feelsFahrenheit *100) /100;
                     }else {
                         degreeSpan.textContent = "°C";
+                        feelsSpan.textContent = degreeSpan.textContent;
                         temperatureDegree.textContent = celsius;
+                        feelsTemperature.textContent = feelsCelsius;
                         //temperatureDegree.textContent = temp;
                     }
                 })
